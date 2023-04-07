dccd926ff0ce61f0293a4e1400c1f65ef1342bf1
Solomon Tadesse <122595521+solabest@users.noreply.github.com>
2023-04-07 06:23:47 -0800

    Add files via upload
    Question 2 Pattern C

---
+++ Question 2 C pattern/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+    int i,c=35,n;
+    for(int i=0;i<=9;i++){
+        for(int j=i;j>=0;j--){
+            cout<<j<<" ";
+        }
+
+        if(i<9){
+            n=c;
+
+            cout<<setw(n);
+
+            }
+        c-=4;
+        if(i==9){
+                for(int z=1;z<=9;z++){
+                    cout<<z<<" ";
+                }
+             }
+             else{
+                    for (int k=0;k<=i;k++){
+
+
+                cout<<k<<" ";
+
+        }
+
+             }
+
+
+        cout<<endl;
+        }
+
+
+    c=3;
+
+    for(int i=8;i>=0;i--){
+        for(int j=i;j>=0;j--){
+            cout<<j<<" ";
+        }
+        if(i!=9){
+            n=c;
+            cout<<setw(n);
+        }
+        c+=4;
+
+        for(int k=0;k<=i;k++){
+            cout<<k<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+
+
+
+
+return 0;
+    }
+
